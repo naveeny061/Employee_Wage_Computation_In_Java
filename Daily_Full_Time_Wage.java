f79e40e6109250bf8a172c5587fa333c3f59c131
naveeny061 <naveeny061@gmail.com>
2020-10-27 17:45:16 +0530

    Add: Part Time Wage

--- Daily_Full_Time_Wage.java
+++ Daily_Full_Time_Wage.java
@@ -1,8 +1,11 @@
 public class Daily_Full_Time_Wage{
 	int Wage_Per_Hour=20;
 	int Full_Day_Hours=8;
-	int Daily_Wage=Wage_Per_Hour*Full_Day_Hours;
+	int Part_Time_Hours=4;
+	int Daily_Wage_Full=Wage_Per_Hour*Full_Day_Hours;
+	int Daily_Wage_Part_Time=Wage_Per_Hour*Part_Time_Hours;
 	void Show_Wages(){
-		System.out.println("Daily employee wage for full time ="+Daily_Wage);
+		System.out.println("Daily employee wage for full time ="+Daily_Wage_Full);
+		System.out.println("Daily employee Wage for Part time ="+Daily_Wage_Part_Time);
 		}
 }
