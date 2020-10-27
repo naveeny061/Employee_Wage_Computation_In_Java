9ef1c4a2e8cbd4ad38cbe260b2c562b52a5fb928
naveeny061 <naveeny061@gmail.com>
2020-10-27 17:34:56 +0530

    Add: Daily Wage Calculation Full Time

---
+++ Daily_Full_Time_Wage.java
@@ -0,0 +1,8 @@
+public class Daily_Full_Time_Wage{
+	int Wage_Per_Hour=20;
+	int Full_Day_Hours=8;
+	int Daily_Wage=Wage_Per_Hour*Full_Day_Hours;
+	void Show_Wages(){
+		System.out.println("Daily employee wage for full time ="+Daily_Wage);
+		}
+}
--- Employee_Wage_Computation.java
+++ Employee_Wage_Computation.java
@@ -1,8 +1,10 @@
-public class Employee_Wage_Computation extends Attendence{
+public class Employee_Wage_Computation {
 
 	public static void main(String [] arg){
 		System.out.println("Welcome To Employee Wage Computation Program");
 		Attendence employee=new Attendence();
 		employee.calculate_attendence();
+		Daily_Full_Time_Wage Daily_Full_Wage=new Daily_Full_Time_Wage();
+		Daily_Full_Wage.Show_Wages();
 		}
 }
