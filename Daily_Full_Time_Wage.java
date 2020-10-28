097529441ded815e795ad8a0e082cadd415c1e7d
naveeny061 <naveeny061@gmail.com>
2020-10-28 17:13:13 +0530

    Refactoring Code For Multiple Companies

--- Daily_Full_Time_Wage.java
+++ Daily_Full_Time_Wage.java
@@ -1,30 +1,25 @@
 import java.util.Random;
 public class Daily_Full_Time_Wage{
-	int Wage_Per_Hour=20;
-	int Full_Day_Hours=8;
-	int Part_Time_Hours=4;
-	int Daily_Wage_Full=Wage_Per_Hour*Full_Day_Hours;
-	int Daily_Wage_Part_Time=Wage_Per_Hour*Part_Time_Hours;
-	int Total_Wage=0;
-	int Total_Days=0;
 	int Working_Hours=0;
-	Random random=new Random();
-	void Show_Wages(){
+	int Total_Days=0;
+	int Total_Wage=0;
+	void Show_Wages(int x,int y,int z){
+		Random random=new Random();
 		switch(random.nextInt(2)){
                 	case 0:
-			while(Total_Days<20 && Working_Hours<100 ){
+			while(Total_Days<y && Working_Hours<z ){
 				Total_Days++;
 				Working_Hours+=8;
-				Total_Wage+=Daily_Wage_Full;
+				Total_Wage+=(x*8);
 			}
 			System.out.println("Total Day="+Total_Days+" Total Working Hours"+Working_Hours);
 			System.out.println("Full Time Employee Total Wage ="+Total_Wage);
 			break;
 			case 1:
-			while(Total_Days<20 && Working_Hours<100 ){
+			while(Total_Days<y && Working_Hours<z ){
                                 Total_Days++;
                                 Working_Hours+=4;
-				Total_Wage+=Daily_Wage_Part_Time;
+				Total_Wage+=(x*4);
 			}
 			System.out.println("Total Day="+Total_Days+" Total Working Hours"+Working_Hours);
 			System.out.println("Part Time Employee Total Wage ="+Total_Wage);
--- Employee_Wage_Computation.java
+++ Employee_Wage_Computation.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Employee_Wage_Computation {
 
 	public static void main(String [] arg){
@@ -5,6 +6,13 @@ public class Employee_Wage_Computation {
 		Attendence employee=new Attendence();
 		employee.calculate_attendence();
 		Daily_Full_Time_Wage Daily_Full_Wage=new Daily_Full_Time_Wage();
-		Daily_Full_Wage.Show_Wages();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Wage Per Hour");
+		int Wages_Per_Hour=sc.nextInt();
+		System.out.println("Enter Number Of Working Days");
+		int Working_Days=sc.nextInt();
+		System.out.println("Enter Working Hour Per Month");
+		int Working_Hour_Per_Month=sc.nextInt();
+		Daily_Full_Wage.Show_Wages(Wages_Per_Hour,Working_Days,Working_Hour_Per_Month);
 		}
 }
