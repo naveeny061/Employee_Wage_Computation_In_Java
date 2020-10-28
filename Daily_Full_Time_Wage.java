e673d6f1ccce6c5701b7555eb66138a210ca1b43
naveeny061 <naveeny061@gmail.com>
2020-10-28 14:48:18 +0530

    Modify: Total Wages Based On Conditions

--- Daily_Full_Time_Wage.java
+++ Daily_Full_Time_Wage.java
@@ -5,19 +5,29 @@ public class Daily_Full_Time_Wage{
 	int Part_Time_Hours=4;
 	int Daily_Wage_Full=Wage_Per_Hour*Full_Day_Hours;
 	int Daily_Wage_Part_Time=Wage_Per_Hour*Part_Time_Hours;
-	int Daily_Wage_Full_Month=Daily_Wage_Full*20;
-        int Daily_Wage_Part_Time_Month= Daily_Wage_Part_Time*20;
-
+	int Total_Wage=0;
+	int Total_Days=0;
+	int Working_Hours=0;
 	Random random=new Random();
 	void Show_Wages(){
 		switch(random.nextInt(2)){
                 	case 0:
-			System.out.println("Daily employee wage for full time ="+Daily_Wage_Full);
-			System.out.println("Employee Wage for Month ="+ Daily_Wage_Full_Month);
+			while(Total_Days<20 && Working_Hours<100 ){
+				Total_Days++;
+				Working_Hours+=8;
+				Total_Wage+=Daily_Wage_Full;
+			}
+			System.out.println("Total Day="+Total_Days+" Total Working Hours"+Working_Hours);
+			System.out.println("Full Time Employee Total Wage ="+Total_Wage);
 			break;
 			case 1:
-                	System.out.println("Daily employee Wage for Part time ="+Daily_Wage_Part_Time);
-			System.out.println("Employee Wage for Month ="+ Daily_Wage_Part_Time_Month);
+			while(Total_Days<20 && Working_Hours<100 ){
+                                Total_Days++;
+                                Working_Hours+=4;
+				Total_Wage+=Daily_Wage_Part_Time;
+			}
+			System.out.println("Total Day="+Total_Days+" Total Working Hours"+Working_Hours);
+			System.out.println("Part Time Employee Total Wage ="+Total_Wage);
 		}
 	}
 }
