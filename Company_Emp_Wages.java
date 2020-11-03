836a0b561e57c167c846e885e3a88e889da38798
naveeny061 <naveeny061@gmail.com>
2020-11-03 18:03:43 +0530

    Refactor:Emp_Wage_Builder and Company_Emp_Wages to use interface approach

--- Company_Emp_Wages.java
+++ Company_Emp_Wages.java
@@ -1,8 +1,5 @@
 import java.util.Random;
-
 public class Company_Emp_Wages {
-	int Part_Time = 0;
-	int Full_Time = 1;
 	String Company_Name;
 	int Emp_Rate_Per_Hour;
 	int Emp_Number_Of_Working_Days;
@@ -15,28 +12,8 @@ public class Company_Emp_Wages {
 		this.Emp_Number_Of_Working_Days = Emp_Number_Of_Working_Days;
 		this.Max_Hours_Per_Month = Max_Hours_Per_Month;
 	}
-	int Compute_Emp_Wage() {
-		int Emp_Hours = 0;
-		int Total_Emp_Hours = 0;
-		int Total_Working_Days = 0;
-		while (Total_Emp_Hours <= Max_Hours_Per_Month && Total_Working_Days < Emp_Number_Of_Working_Days) {
-			Total_Working_Days++;
-			Random r1 = new Random();
-			int Emp_Check = r1.nextInt(2);
-			switch (Emp_Check) {
-			case 0:
-				Emp_Hours = 4;
-				break;
-			case 1:
-				Emp_Hours = 8;
-				break;
-			default:
-				Emp_Hours = 0;
-			}
-			Total_Emp_Hours += Emp_Hours;
-		}
-		Total_Emp_Wage = Total_Emp_Hours * Emp_Rate_Per_Hour;
-		return Total_Emp_Wage;
+	public void Set_Total_Emp_Wage(int Total_Emp_Wage) {
+		this.Total_Emp_Wage=Total_Emp_Wage;
 	}
 	public String toString() {
 		return "Total Emp Wage For Company " + Company_Name + " is " + Total_Emp_Wage;
--- Emp_Wage_Builder.java
+++ Emp_Wage_Builder.java
@@ -1,35 +1,54 @@
-import java.util.Scanner;
+import java.util.Random;
 public class Emp_Wage_Builder {
+	private int Part_Time = 0;
+	private int Full_Time = 1;
+	private int Number_Of_Company = 0;
+	private Company_Emp_Wages[] company_Emp_Wage_Array;
+	Emp_Wage_Builder() {
+		company_Emp_Wage_Array = new Company_Emp_Wages[5];
+	}
+	private void Add_Company_Emp_Wage(String Company_name, int Emp_Rate_Per_Hour, int Emp_Number_Of_Working_Days,
+			int Max_Hours_Per_Month) {
+		company_Emp_Wage_Array[Number_Of_Company] = new Company_Emp_Wages(Company_name, Emp_Rate_Per_Hour,
+				Emp_Number_Of_Working_Days, Max_Hours_Per_Month);
+		Number_Of_Company++;
+	}
+	private void Compute_Emp_Wage() {
+		for (int i = 0; i < Number_Of_Company; i++) {
+			company_Emp_Wage_Array[i].Set_Total_Emp_Wage(this.Compute_Emp_Wage(company_Emp_Wage_Array[i]));
+			System.out.println(company_Emp_Wage_Array[i]);
+		}
+	}
+	private int Compute_Emp_Wage(Company_Emp_Wages company_Emp_Wage) {
+		int Emp_Hours = 0;
+		int Total_Emp_Hours = 0;
+		int Total_Working_Days = 0;
+		while (Total_Emp_Hours <= company_Emp_Wage.Max_Hours_Per_Month
+				&& Total_Working_Days < company_Emp_Wage.Emp_Number_Of_Working_Days) {
+			Total_Working_Days++;
+			Random r1 = new Random();
+			int Emp_Check = r1.nextInt(2);
+			switch (Emp_Check) {
+			case 0:
+				Emp_Hours = 4;
+				break;
+			case 1:
+				Emp_Hours = 8;
+				break;
+			default:
+				Emp_Hours = 0;
+			}
+
+			Total_Emp_Hours += Emp_Hours;
+			System.out.println("Days=" + Total_Working_Days + "Emp Hours=" + Total_Emp_Hours);
+		}
+		company_Emp_Wage.Total_Emp_Wage = Total_Emp_Hours * company_Emp_Wage.Emp_Rate_Per_Hour;
+		return company_Emp_Wage.Total_Emp_Wage;
+	}
 	public static void main(String arg[]) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of companies");
-		int number = sc.nextInt();
-		int array[];
-		array = new int[number];
-		String array1[];
-		array1 = new String[number];
-		String Company_Name;
-		int Emp_Rate_Per_Hour;
-		int Emp_Number_Of_Working_Days;
-		int Max_Hours_Per_Month;
-		int i = 0;
-		while (i < number) {
-			System.out.println("Enter Company Name ");
-			Company_Name = sc.next();
-			System.out.println("Enter Employee Rate Per Hour ");
-			Emp_Rate_Per_Hour = sc.nextInt();
-			System.out.println("Enter Number Of Working Days ");
-			Emp_Number_Of_Working_Days = sc.nextInt();
-			System.out.println("Enter Max Hours Per Month ");
-			Max_Hours_Per_Month = sc.nextInt();
-			Company_Emp_Wages obj1 = new Company_Emp_Wages(Company_Name, Emp_Rate_Per_Hour, Emp_Number_Of_Working_Days,
-					Max_Hours_Per_Month);
-			array[i] = obj1.Compute_Emp_Wage();
-			array1[i] = obj1.Company_Name;
-			i++;
-		}
-		for (i = 0; i < number; i++) {
-			System.out.println("Company Name-"+array1[i]+"  Total Wage-"+array[i]);
-		}
+			Emp_Wage_Builder obj1 = new Emp_Wage_Builder();
+			obj1.Add_Company_Emp_Wage("Nike",100,20,120);
+			obj1.Add_Company_Emp_Wage("Rebook",120,18,80);
+			obj1.Compute_Emp_Wage();
 	}
 }
